a92ce7084552245c867546d7900d3a9f0b0fa2a7
Czila <lcesar.mk@gmail.com>
2022-07-20 12:00:15 +0200

    route et schema carte

---
+++ controllers/carteController.js
@@ -0,0 +1,68 @@
+const carteSchema = require('../db/models/Carte')
+
+
+const carteController = {
+
+getCartes: (req,res) => {
+    carteSchema.find({}).then((cartes)=>res.send(cartes))
+},
+
+getCarte: (req,res) => {
+    const _id = req.params.id
+    carteSchema.find({_id}).then((carte)=>res.send(carte))
+},
+
+createCarte: async (req, res) => {
+ const {menus, plats} = req.body
+try {
+ const carte = new carteSchema({
+    menus,
+    plats
+ })
+
+ await carte.save()
+ res.send(carte)
+}
+catch(err)
+{
+    res.send(err.message)
+}
+},
+
+updateCarte: async (req,res) => {
+    const _id = req.params.id
+    const {menus, plats} = req.body
+    console.log(req.params.id)
+
+try {
+    const carteUpdate = await carteSchema.findByIdAndUpdate(_id, {
+        menus,
+        plats
+    })
+
+    res.send(carteUpdate)
+}
+
+    catch(err)
+{
+    res.send(err.message)
+}
+},
+
+
+deleteCarte: async (req,res) => {
+    const _id = req.params.id
+
+try {
+    const carteDelete = await carteSchema.deleteOne({_id: _id})
+
+    res.send(carteDelete)
+}
+
+    catch(err)
+{
+    res.send(err.message)
+}
+}
+}
+module.exports = carteController;

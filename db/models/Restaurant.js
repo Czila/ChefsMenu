39507bdb7f30a926d443bb717004e6f1e7387419
benedicteb2054 <benedictebouy@gmail.com>
2022-07-20 13:43:51 +0200

    ajout restaurant

---
+++ db/models/Restaurant.js
@@ -0,0 +1,14 @@
+const {default:mongoose} = require("mongoose")
+
+const restaurantSchema = new mongoose.Schema({
+nom: String,
+adresse: String,
+cp: String,
+ville: String,
+image: String,
+horaire: Date,
+nbTable: Integer,
+idRestaurateur
+})
+
+module.exports = mongoose.model("Restaurant", restaurantSchema) 
---
+++ routes/restaurant.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+const restaurantController = require('../controllers/restaurantController')
+
+
+/* GET users listing. */
+router.get('/', restaurantController.getRestaurant);
+
+router.get('/:id', restaurantController.getRestaurant);
+
+router.post('/', restaurantController.createRestaurant);
+
+router.put('/:id', restaurantController.updateRestaurant);
+
+router.delete('/:id', restaurantController.deleteRestaurant);
+
+
+module.exports = router;
